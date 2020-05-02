6b274938a82fe2a4c12d22f27a545a1b983ac3c4
muhamed <Eng-Muhammed-Samy>
2020-05-02 17:02:24 +0200

    add function body in js file

---
+++ js/remot.js
@@ -0,0 +1,15 @@
+function setNum(a){}
+
+document.getElementById('add').onclick = function (){}
+
+document.getElementById('subtract').onclick = function (){}
+
+document.getElementById('division').onclick = function (){}
+
+document.getElementById('multiplication').onclick = function (){}
+
+document.getElementById('square').onclick = function (){}
+
+document.getElementById('sqrt').onclick = function (){}
+
+document.getElementById('clear').onclick = function (){}
