d6943792f278661a0201be6785f96929187ca6f9
muhamed <Eng-Muhammed-Samy>
2020-05-04 00:18:12 +0200

    add equal function

--- js/remot.js
+++ js/remot.js
@@ -5,28 +5,28 @@ function setNum(a){
 }
 
 document.getElementById('add').onclick = function (){
-    var value = document.getElementById('screen').value ;
+    value = document.getElementById('screen').value ;
     operator = '+';
     document.getElementById('txt').value = value +" "+ operator +" ";
     document.getElementById('screen').value = " ";
 }
 
 document.getElementById('subtract').onclick = function (){
-    var value = document.getElementById('screen').value ;
+    value = document.getElementById('screen').value ;
     operator = '-';
     document.getElementById('txt').value = value +" "+ operator +" ";
     document.getElementById('screen').value = " ";
 }
 
 document.getElementById('division').onclick = function (){
-    var value = document.getElementById('screen').value ;
+    value = document.getElementById('screen').value ;
     operator = '/';
     document.getElementById('txt').value = value +" "+ operator +" ";
     document.getElementById('screen').value = " ";
 }
 
 document.getElementById('multiplication').onclick = function (){
-    var value = document.getElementById('screen').value ;
+    value = document.getElementById('screen').value ;
     operator = '*';
     document.getElementById('txt').value = value +" "+ operator +" ";
     document.getElementById('screen').value = " ";
@@ -40,6 +40,29 @@ document.getElementById('square').onclick = function (){
 document.getElementById('sqrt').onclick = function (){
     value = document.getElementById('screen').value;
     document.getElementById('screen').value = Math.sqrt(value);
+}
+
+document.getElementById('sum').onclick = function (){
+    var num1 = parseInt(value);
+    var num2 = parseInt(document.getElementById('screen').value); 
+    
+    switch(operator){
+        case '+':
+            document.getElementById('screen').value = num1 + num2 ;
+            break;
+        case '-':
+            document.getElementById('screen').value = num1 - num2 ;
+            break;
+        case '*':
+            document.getElementById('screen').value = num1 * num2 ;
+            break;
+        case '/':
+            if(num2 == 0)
+                document.getElementById('screen').value = '∞' ;
+            else
+                document.getElementById('screen').value = num1 / num2 ;
+            break;
+    }
 }
 
 document.getElementById('clear').onclick = function (){
